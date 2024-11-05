0d37aba3d2474829e814c1c1535c8950bbc112c6
Kevin Lawrence <klawrence@simplemachines.com.au>
2024-11-05 17:16:21 +1100

    Updated dependencies by adding the spark dependency - need to fix the other deps versions and fix the testing suite bit in `build.gradle`

--- app/src/main/scala/proj/flightdata/App.scala
+++ app/src/main/scala/proj/flightdata/App.scala
@@ -4,11 +4,8 @@
 package proj.flightdata
 
 object App extends App {
-  def main(args: Array[String]): Unit = {
-    println(greeting())
-  }
 
-
+  println(greeting())
 
   def greeting(): String = "Hello, world!"
 }
